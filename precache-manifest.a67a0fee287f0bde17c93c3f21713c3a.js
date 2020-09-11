854f565ee40e6f5e72301fe83ba5439654f69ef6
Andrii Tsvirko <a.tsvirko@ukr.net>
2020-09-11 20:35:04 +0300

    Updates

---
+++ precache-manifest.a67a0fee287f0bde17c93c3f21713c3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf6238e3bf8db86f2becd9569dcfbbe",
+    "url": "/reactPortfolioSite/index.html"
+  },
+  {
+    "revision": "c37b2bc65aa6f40663fb",
+    "url": "/reactPortfolioSite/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "15bdfd271f69c35c8a1e",
+    "url": "/reactPortfolioSite/static/css/main.6e167083.chunk.css"
+  },
+  {
+    "revision": "c37b2bc65aa6f40663fb",
+    "url": "/reactPortfolioSite/static/js/2.10493819.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/reactPortfolioSite/static/js/2.10493819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15bdfd271f69c35c8a1e",
+    "url": "/reactPortfolioSite/static/js/main.344d196c.chunk.js"
+  },
+  {
+    "revision": "6b2ac82acb8f770ed7e0",
+    "url": "/reactPortfolioSite/static/js/runtime-main.6b740939.js"
+  }
+]);
