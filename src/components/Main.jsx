df0c370e5908ed65ed4d6533ac6dcb1e76d3e4b8
Andrii Tsvirko <a.tsvirko@ukr.net>
2020-09-16 10:16:48 +0300

    fixed browserRouter bug

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,25 +8,11 @@ import Projects from "./Projects/Projects";
 export const Main = () => {
   return (
     <Switch>
-      <Route exact path="/" component={LandingPage} />
-      {/* <Route exact path="/home" component={LandingPage} /> */}
+      <Route path="/reactPortfolioSite" component={LandingPage} />
+      <Route exact path="/home" component={LandingPage} />
       <Route path="/about" component={AboutMe} />
       <Route path="/contacts" component={Contacts} />
       <Route path="/projects" component={Projects} />
-      <Route
-        render={() => (
-          <h1
-            style={{
-              color: "green",
-              textAlign: "center",
-              marginTop: "150px",
-              fontSize: "80px",
-            }}
-          >
-            Page not found
-          </h1>
-        )}
-      />
     </Switch>
   );
 };
